b3b4201a8c9f2d8262130a364f3e2cf8731a9853
Taff-Lawley <taff.lawley@gmail.com>
2013-01-01 12:51:15 +0100

    initial

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'login' => 'cake',
+		'password' => 'EYt9UtzxPYjpV2zr',
+		'database' => 'projectmanagement',
+	);
+}
